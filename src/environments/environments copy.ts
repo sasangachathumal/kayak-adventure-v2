6277d2994bb0717a9af60ceca8c306b3f5c1332f
Sasanga Chathumal <chathumal.sasanga02@gmail.com>
2024-02-24 23:00:35 +0530

    update

---
+++ src/environments/environments copy.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: '<projectId>',
+    appId: '',
+    databaseURL: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: ''
+  },
+  AWS: {
+    accessKey: '',
+    secretAccessKey: '',
+    region: '',
+    bucketKey: '',
+    paths: {
+      video: '/videos',
+      image: '/image',
+      files: '/file',
+    }
+  }
+};
